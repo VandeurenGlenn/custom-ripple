e5d88094b066f10afb08d30766aadacac59e5598
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-10-18 01:49:00 +0200

    :mega: Port to Backed 0.9.X

---
+++ custom-ripple.js
@@ -0,0 +1,42 @@
+import {define} from '../backed/src/utils.js';
+import LitMixin from '../backed/mixins/lit-mixin.js';
+
+export default define(class CustomRipple extends LitMixin(HTMLElement) {
+  constructor() {
+    super();
+  }
+	/**
+	 * Runs the ripple
+	 */
+	run() {
+		this.classList.add('run');
+		setTimeout(() => {
+			this.classList.remove('run');
+		}, 320);
+	}
+
+  render() {
+    return html`
+    <style>
+      :host {
+        background: rgba(0,0,0,0.12);
+        position: absolute;
+        opacity: 0;
+        right: 0;
+        bottom: 0;
+        border-radius: var(--custom-ripple-radius, 50%);
+        outline: none;
+        transition: transform cubic-bezier(0.22, 0.61, 0.36, 1) 600ms;
+        transform: translate3d(0,0,0);
+      }
+      :host(.run) {
+        opacity: 1;
+        top: 0;
+        left: 0;
+        transition: transform cubic-bezier(0, 0, 0.2, 1) 400ms;
+        transform: translate3d(0,0,0);
+      }
+    </style>
+    `;
+  }
+});
